81f3d8ff71616ca0bfa9db907c71339073421392
shivam-kumar-79 <shivamkumarattri79@gmail.com>
2025-10-03 22:44:51 +0530

    Add 112A String question sol 800

---
+++ 800/A112.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class A112{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        int s=s1.compareTo(s2);
+        System.out.println(s);
+        sc.close();
+    }
+}
